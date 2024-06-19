6de9c578d2f219991bbb5e099a6bf923d59b94a7
Ehtisham-Umar-Ansari <ehtishamansari05@gmail.com>
2024-06-20 00:55:49 +0530

    Third commit: Forgot to add other files and activities

---
+++ app/src/main/java/com/example/eadevelops/pyqslu/activities/UploadNotesActivity.kt
@@ -0,0 +1,134 @@
+package com.example.eadevelops.pyqslu.activities
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.documentfile.provider.DocumentFile
+import com.example.eadevelops.pyqslu.R
+import com.example.eadevelops.pyqslu.databinding.ActivityUploadNotesBinding
+import com.example.eadevelops.pyqslu.models.PDF
+import com.example.eadevelops.pyqslu.utils.PDF_FOLDER
+import com.example.eadevelops.pyqslu.utils.PDFs
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+
+class UploadNotesActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityUploadNotesBinding.inflate(layoutInflater)
+    }
+    private var pdfFileUri : Uri? = null
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()){uri->
+        pdfFileUri = uri
+        val fileName = uri?.let { DocumentFile.fromSingleUri(this, it) }?.name
+        if(fileName != null){
+            binding.fileName.text = fileName.toString()
+            binding.fileName.setTextColor(resources.getColor(R.color.green))
+        }
+        binding.progressBar.visibility = View.GONE
+    }
+    private lateinit var storageReference : StorageReference
+    private lateinit var dataBaseReference: DatabaseReference
+
+    override fun onResume() {
+        super.onResume()
+
+        var objects = resources.getStringArray(R.array.Course)
+        var arrayAdapter = ArrayAdapter(this, R.layout.dropdown_menu, objects)
+        binding.course.setAdapter(arrayAdapter)
+
+        objects = resources.getStringArray(R.array.Branch)
+        arrayAdapter = ArrayAdapter(this, R.layout.dropdown_menu, objects)
+        binding.branch.setAdapter(arrayAdapter)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        storageReference = FirebaseStorage.getInstance().reference.child(PDF_FOLDER)
+        dataBaseReference = FirebaseDatabase.getInstance().reference.child(PDFs).child("Notes")
+
+        binding.choosePdf.setOnClickListener{
+            binding.progressBar.visibility = View.VISIBLE
+            pdfFileUri = null
+            launcher.launch("application/pdf")
+            if(pdfFileUri == null){
+                binding.fileName.text = "No File selected"
+                binding.fileName.setTextColor(resources.getColor(R.color.red))
+            }
+        }
+
+        binding.upload.setOnClickListener {
+            if(pdfFileUri != null){
+                binding.progressBar.visibility = View.VISIBLE
+                uploadPdf()
+            }else
+                Toast.makeText(this, "Please Select a pdf file", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // This is the code to upload pdf to firebase.
+    private fun uploadPdf(){
+
+        if(binding.course.text.toString() == "" || binding.branch.text.toString() == "" ||
+            binding.sem.editText?.text.toString() == "" || binding.subjectName.editText?.text.toString() == "" ||
+            binding.year.editText?.text.toString() == ""){
+
+            Toast.makeText(this, "All fields required", Toast.LENGTH_SHORT).show()
+
+        }else{
+
+            val fileName = binding.course.text.toString()+binding.branch.text.toString()+binding.sem.editText?.text.toString()+
+                    binding.unit.editText?.text.toString()+binding.subjectName.editText?.text.toString()+binding.year.editText?.text.toString()
+
+            val storage = Firebase.storage
+            val fileReference = storage.reference.child("$PDF_FOLDER/$fileName")
+            fileReference.metadata.addOnSuccessListener {
+                Toast.makeText(this, "PDF already Exist", Toast.LENGTH_SHORT).show()
+                binding.progressBar.visibility = View.GONE
+            }.addOnFailureListener {
+                uploadDoc(fileName)
+            }
+        }
+    }
+
+    private fun uploadDoc(fileName: String) {
+        val mStorageRef = storageReference.child(fileName)
+
+        pdfFileUri?.let {uri->
+            mStorageRef.putFile(uri).addOnSuccessListener {
+                mStorageRef.downloadUrl.addOnSuccessListener {downloadedURI->
+
+                    val PDFFile = PDF(fileName, downloadedURI.toString())
+
+                    dataBaseReference.child(fileName).setValue(PDFFile).addOnCompleteListener {task->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Uploaded Successfully", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                    }.addOnFailureListener {
+                        Toast.makeText(this, it.message.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/eadevelops/pyqslu/activities/drawer_activities/NotesActivity.kt
@@ -0,0 +1,62 @@
+package com.example.eadevelops.pyqslu.activities.drawer_activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.eadevelops.pyqslu.R
+import com.example.eadevelops.pyqslu.activities.RetrievePYQ
+import com.example.eadevelops.pyqslu.databinding.ActivityNotesBinding
+
+class NotesActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityNotesBinding.inflate(layoutInflater)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        var objects = resources.getStringArray(R.array.Course)
+        var arrayAdapter = ArrayAdapter(this, R.layout.dropdown_menu, objects)
+        binding.course.setAdapter(arrayAdapter)
+
+        objects = resources.getStringArray(R.array.Branch)
+        arrayAdapter = ArrayAdapter(this, R.layout.dropdown_menu, objects)
+        binding.branch.setAdapter(arrayAdapter)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.showAll.setOnClickListener {
+            startActivity(Intent(this, RetrievePYQ::class.java))
+        }
+
+        binding.get.setOnClickListener {
+            if(binding.course.text.toString() != "" && binding.branch.text.toString() != ""
+                && binding.sem.editText?.text.toString() != "" && binding.unit.editText?.text.toString() != ""
+                /*&& binding.subjectName.editText?.text.toString() != ""*/){
+                val intent = Intent(this, RetrievePYQ::class.java)
+                    .putExtra("fileName", binding.course.text.toString()+binding.branch.text.toString()+
+                            binding.sem.editText?.text.toString()+binding.unit.editText?.text.toString()+
+                            binding.subjectName.editText?.text.toString())
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
